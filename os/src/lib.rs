357305ad2319a63c635ff15c2f34b70bdd292ccc
Cliff L. Biffle <code@cliffle.com>
2023-06-28 23:32:18 -0700

    Elaborate os module docs a bit.

--- os/src/lib.rs
+++ os/src/lib.rs
@@ -1,11 +1,34 @@
-//! A simple RTOS based around Rust `Future`s.
+//! A simple `async` RTOS based around Rust `Future`s.
 //!
 //! This provides a minimal operating environment for running async Rust code on
 //! ARM Cortex-M microprocessors, plus some useful doodads and gizmos.
 //!
+//! # `lilos` design principles
+//!
+//! 1. Be compact. Avoid doing things that increase the minimum size of a useful
+//!    application. In particular, try to avoid designing APIs that need
+//!    internal asserts/panics, because those are quite costly in text size.
+//!
+//! 2. No magic. `lilos` doesn't use proc macros or other tricks to hide what
+//!    it's doing. Some (normal) macros are available, but only as shorthand,
+//!    and we'll explain how to do the thing without the macros. This is
+//!    important, both for transparency, but also because you might need to
+//!    customize what's happening, and you can't do that if we hide it in a box.
+//!
+//! 3. Be portable. `lilos` doesn't depend on any vendor-specific hardware, and
+//!    can run on any ARM Cortex-M processor. This means you don't need to wait
+//!    for `lilos` to be ported to your processor. (`lilos` is also not
+//!    inherently ARM-specific -- I just don't have a lot of non-ARM dev
+//!    hardware. Suggestions welcome!)
+//!
+//! 4. Be predictable. `lilos` doesn't use dynamic memory allocation, and the
+//!    task polling behavior is well-defined in the executor. While you can
+//!    certainly build unpredictable programs on top of `lilos`, we want to give
+//!    you a solid predictable foundation.
+//!
 //! # About the OS
 //!
-//! This OS is designed around the notion of a fixed set of concurrent tasks
+//! `lilos` is designed around the notion of a fixed set of concurrent tasks
 //! that run forever. To use the OS, your application startup routine calls
 //! [`exec::run_tasks`], giving it an array of tasks you've defined; `run_tasks`
 //! never returns.
@@ -36,19 +59,25 @@
 //!
 //!     loop {
 //!         gpio.bsrr.write(|w| w.bs12().set_bit());
-//!         os::exec::sleep_for(PERIOD).await;
+//!         lilos::exec::sleep_for(PERIOD).await;
 //!         gpio.bsrr.write(|w| w.br12().set_bit());
-//!         os::exec::sleep_for(PERIOD).await;
+//!         lilos::exec::sleep_for(PERIOD).await;
 //!     }
 //! }
 //! ```
 //!
 //! (Note: the natural way to write that function would be with a return type of
-//! `!`, but doing this requires unstable toolchain features inside the OS, so
-//! we rely on `core::convert::Infallible` instead in this version.)
+//! `!`, but doing this requires the unstable toolchain, so we rely on
+//! `core::convert::Infallible` instead in this version.)
 //!
-//! Because `Future`s can be _composed_, the fixed set of OS tasks can drive a
-//! _dynamic_ set of program `Future`s.
+//! # Composition and dynamic behavior
+//!
+//! The notion of a fixed set of tasks might seem limiting, but it's more
+//! flexible than you might expect. Because `Future`s can be _composed_, the
+//! fixed set of OS tasks can drive a _dynamic_ set of program `Future`s.
+//!
+//! For instance, a task can fork into several concurrent routines using macros
+//! like [`select_biased!`] or [`join!`] from the `futures` crate.
 //!
 //! # Concurrency and interrupts
 //!
@@ -66,7 +95,10 @@
 //! Interrupts are also unmasked whenever the idle processor is woken from
 //! sleep, in order to handle the event that woke it up.
 //!
-//! See the [`exec`][crate::exec] module for more details and some customization
+//! If your application requires tighter interrupt response time, you can
+//! configure the OS at startup to permit preemption of various kinds --
+//! including allowing preemption by only a subset of your interrupts. See the
+//! [`exec`][crate::exec] module for more details and some customization
 //! options.
 //!
 //! # Cancellation
@@ -75,15 +107,38 @@
 //! dropped. `Future`s are typically dropped just after they resolve (often just
 //! after an `await` keyword in the calling code), but it's also possible to
 //! drop a `Future` while it is pending. This can happen explicitly (by calling
-//! [`drop`]), or as a side effect of other operations; for example, the future
-//! returned by
-//! [`select!`](https://docs.rs/futures/0.3/futures/macro.select.html) will drop
-//! any uncompleted futures if you `await` it.
+//! [`drop`]), or as a side effect of other operations; for example, the macro
+//! [`select_biased!`] waits for one future to resolve, and then drops the
+//! others, whether they're done or not.
 //!
 //! This means it's useful to consider what cancellation *means* for any
-//! particular task, and to ensure that its results are what you intend.
+//! particular task, and to ensure that its results are what you intend. There's
+//! more about this in [The Intro Guide] and the technical note on
+//! [Cancellation].
 //!
-//! OS-provided futures attempt to provide useful cancellation behavior.
+//! `lilos` itself tries to make it easier for you to handle cancellation in
+//! your programs, by providing APIs that have reasonable behavior on cancel.
+//! Specifically,
+//!
+//! - Wherever possible, `lilos` futures strive for a _strict_ definition of
+//!   cancel-safety, where dropping a future and retrying the operation that
+//!   produced it is equivalent to not dropping the future, in terms of visible
+//!   side effects. (Obviously doing more work will take more CPU cycles; that's
+//!   not what we mean by side effects.)
+//!
+//! - Where that is not possible (in a few corner-case APIs) `lilos` will
+//!   provide _weak_ cancel-safety, where the behavior of the operation at
+//!   cancellation is well-defined and documented.
+//!
+//! Over time, we're trying to redesign APIs to move things out of the second
+//! category into the first, with the goal of providing a fully cancel-safe OS
+//! API. As far as we can tell nobody's ever done this before, so it might take
+//! a bit. If you have suggestions or ideas, please file an issue!
+//!
+//! [`select_biased!`]: https://docs.rs/futures/latest/futures/macro.select_biased.html
+//! [`join!`]: https://docs.rs/futures/latest/futures/macro.join.html
+//! [The Intro Guide]: https://github.com/cbiffle/lilos/blob/main/doc/intro.adoc
+//! [Cancellation]: https://github.com/cbiffle/lilos/blob/main/doc/cancellation.adoc
 
 #![no_std]
 
