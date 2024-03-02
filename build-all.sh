29bfcf014216761f89827f79f7bd6eb7343fb10f
Cliff L. Biffle <code@cliffle.com>
2024-03-02 12:56:16 -0800

    testsuite/stm32f3: include in build

--- build-all.sh
+++ build-all.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-DIRS="os testsuite/stm32f4 testsuite/stm32g0 examples/*/*"
+DIRS="os testsuite/stm32f4 testsuite/stm32g0 testsuite/stm32f3 examples/*/*"
 
 for d in $DIRS; do
     echo "---- building in $d"
