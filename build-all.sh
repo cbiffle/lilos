947a740c13b20ff38d63524c0ed75a196195f835
Cliff L. Biffle <code@cliffle.com>
2024-04-22 08:53:26 -0700

    Build all OS feature combinations.

--- build-all.sh
+++ build-all.sh
@@ -2,7 +2,25 @@
 
 set -euo pipefail
 
-DIRS="os handoff testsuite/stm32f4 testsuite/stm32g0 testsuite/stm32f3 examples/*/*"
+MODES=(
+	""
+	"--features mutex"
+	"--features spsc"
+	"--features systick"
+	"--features mutex,spsc"
+	"--features mutex,systick"
+	"--features spsc,systick"
+	"--features mutex,spsc,systick"
+	)
+
+for k in ${!MODES[@]}; do
+    echo "---- building OS with: ${MODES[$k]}"
+    pushd os > /dev/null
+    cargo build --no-default-features ${MODES[$k]}
+    popd > /dev/null
+done
+
+DIRS="handoff testsuite/stm32f4 testsuite/stm32g0 testsuite/stm32f3 examples/*/*"
 
 for d in $DIRS; do
     echo "---- building in $d"
