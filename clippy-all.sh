6739254321f954af3086a4ac10812dd3ea33017b
Cliff L. Biffle <code@cliffle.com>
2024-04-22 16:11:21 -0700

    Add Clippy in CI.

---
+++ clippy-all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+DIRS="os handoff testsuite/stm32f4 testsuite/stm32g0 testsuite/stm32f3 examples/*/*"
+
+for d in $DIRS; do
+    echo "---- clipping in $d"
+    pushd $d > /dev/null
+    cargo clippy -- --deny warnings
+    popd > /dev/null
+done
