b8516cc33f682d9fe971b0def6790e83721eff2c
Cliff L. Biffle <code@cliffle.com>
2024-04-22 09:25:40 -0700

    testsuite: fix insert_and_wait_with_cleanup test
    It turns out, I broke this with my fix to list recently, and apparently
    forgot to run it. Tee hee.
    
    It broke because it was testing the broken behavior I was fixing: the
    part where list would insert nodes eagerly without requiring the
    insert_and_wait future to be pinned and polled.
    
    It is now testing the fixed behavior (and passes).

--- testsuite/src/list.rs
+++ testsuite/src/list.rs
@@ -1,4 +1,6 @@
-use core::pin::Pin;
+#![allow(clippy::bool_assert_comparison)]
+
+use core::pin::{Pin, pin};
 use core::cell::Cell;
 
 use lilos::{create_list, create_node};
@@ -35,6 +37,14 @@ pub async fn test_insert_and_wait() {
     // Drop list mutability. TODO: should create_list even return a Pin<&mut>?
     let list = list.into_ref();
 
+    // Insertion must not happen eagerly, it must wait for the insert future to
+    // be pinned and polled.
+    {
+        let _fut = list.insert_and_wait(node.as_mut());
+        // Should not be able to wake it!
+        assert_eq!(list.wake_one(), false);
+    }
+
     futures::join! {
         async {
             // Check that we can insert...
@@ -57,7 +67,7 @@ pub async fn test_insert_and_wait() {
 
     // And just for funsies
     {
-        let fut = core::pin::pin!(list.insert_and_wait(node.as_mut()));
+        let fut = pin!(list.insert_and_wait(node.as_mut()));
         let _ = futures::poll!(fut); // polled exactly once but not woken
     }
     assert!(node.is_detached()); // still works?
@@ -80,13 +90,27 @@ pub async fn test_insert_and_wait_with_cleanup() {
     // Should not be called if the node was detached at drop.
     assert!(!cleanup_called.get());
 
-    // Try again, this time with detachin'
-    let fut = list.insert_and_wait_with_cleanup(
-        node.as_mut(),
-        || cleanup_called.set(true),
-    );
-    assert!(list.wake_one());
-    assert!(!cleanup_called.get()); // not yet
-    drop(fut); // never polled
-    assert!(cleanup_called.get()); // now
+    // Testing the cleanup behavior is slightly subtle: we need to activate the
+    // logic for the case where...
+    // - The node has successfully been inserted
+    // - And then detached
+    // - But hasn't been polled to discover this.
+    //
+    // Once the node has been pinned it becomes hard to drop explicitly, but we
+    // can do it with a scope:
+    {
+        let mut fut = pin!(list.insert_and_wait_with_cleanup(
+                node.as_mut(),
+                || cleanup_called.set(true),
+                ));
+        // Poll the insert future to cause the node to link up.
+        let _ = futures::poll!(fut.as_mut());
+        assert_eq!(cleanup_called.get(), false); // not yet
+                                                 // Detach it from the list.
+        assert!(list.wake_one());
+        assert_eq!(cleanup_called.get(), false); // not yet
+
+        // dropped without poll
+    }
+    assert_eq!(cleanup_called.get(), true); // now
 }
