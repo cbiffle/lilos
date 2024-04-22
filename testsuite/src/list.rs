16fe6c1050e5554cb9b1aee7023fce2b96598269
Cliff L. Biffle <code@cliffle.com>
2024-04-22 09:47:40 -0700

    testsuite: split list tests and improve coverage
    As far as I know the only genuinely new test here is
    no_fire_if_polled_after_detach. But breaking it up into several test
    cases keeps them from accidentally sharing state, which seems good.

--- testsuite/src/list.rs
+++ testsuite/src/list.rs
@@ -1,3 +1,5 @@
+//! Tests for the `List` and `Node` types.
+
 #![allow(clippy::bool_assert_comparison)]
 
 use core::pin::{Pin, pin};
@@ -11,11 +13,15 @@ pub async fn test_node_basics() {
     // Node type is what we expect?
     let node: Pin<&mut Node<()>> = node;
     
-    assert!(node.is_detached());
-    node.as_ref().detach();
-    assert!(node.is_detached());
+    assert!(node.is_detached(), "node must begin as detached");
 
-    // And, detached node drop
+    // Detach must be safe to call on an already-detached node (idempotent).
+    node.as_ref().detach();
+
+    assert!(node.is_detached(), "node must remain detached if not inserted");
+
+    // And, detached node drop -- mostly just checks to see if it inadvertently
+    // panics!
 }
 
 pub async fn test_list_basics() {
@@ -25,17 +31,18 @@ pub async fn test_list_basics() {
 
     // Make sure these don't, like, assert on an empty list or anything
     list.as_ref().wake_all();
-    list.as_ref().wake_one();
+    assert_eq!(list.as_ref().wake_one(), false,
+        "wake_one on an empty list must return false");
 
     // And, detached list drop.
 }
 
-pub async fn test_insert_and_wait() {
+pub async fn test_insert_and_wait_not_eager() {
     create_list!(list);
-    create_node!(node, (), lilos::exec::noop_waker());
-
     // Drop list mutability. TODO: should create_list even return a Pin<&mut>?
     let list = list.into_ref();
+
+    create_node!(node, (), lilos::exec::noop_waker());
 
     // Insertion must not happen eagerly, it must wait for the insert future to
     // be pinned and polled.
@@ -44,6 +51,16 @@ pub async fn test_insert_and_wait() {
         // Should not be able to wake it!
         assert_eq!(list.wake_one(), false);
     }
+
+    assert_eq!(node.is_detached(), true);
+}
+
+pub async fn test_insert_and_wait_wake_one() {
+    create_list!(list);
+    // Drop list mutability. TODO: should create_list even return a Pin<&mut>?
+    let list = list.into_ref();
+
+    create_node!(node, (), lilos::exec::noop_waker());
 
     futures::join! {
         async {
@@ -59,6 +76,14 @@ pub async fn test_insert_and_wait() {
             }
         },
     };
+}
+
+pub async fn test_insert_and_wait_cancel_behavior() {
+    create_list!(list);
+    // Drop list mutability. TODO: should create_list even return a Pin<&mut>?
+    let list = list.into_ref();
+
+    create_node!(node, (), lilos::exec::noop_waker());
 
     // Let's check my assertion about cancel behavior, shall we?
     let fut = list.insert_and_wait(node.as_mut());
@@ -73,14 +98,14 @@ pub async fn test_insert_and_wait() {
     assert!(node.is_detached()); // still works?
 }
 
-pub async fn test_insert_and_wait_with_cleanup() {
+pub async fn test_iawwc_no_fire_if_never_polled() {
     create_list!(list);
-    create_node!(node, (), lilos::exec::noop_waker());
-
     // Drop list mutability. TODO: should create_list even return a Pin<&mut>?
     let list = list.into_ref();
 
+    create_node!(node, (), lilos::exec::noop_waker());
     let cleanup_called = Cell::new(false);
+
     let fut = list.insert_and_wait_with_cleanup(
         node.as_mut(),
         || cleanup_called.set(true),
@@ -89,6 +114,41 @@ pub async fn test_insert_and_wait_with_cleanup() {
     drop(fut);
     // Should not be called if the node was detached at drop.
     assert!(!cleanup_called.get());
+}
+
+pub async fn test_iawwc_no_fire_if_polled_after_detach() {
+    create_list!(list);
+    // Drop list mutability. TODO: should create_list even return a Pin<&mut>?
+    let list = list.into_ref();
+
+    create_node!(node, (), lilos::exec::noop_waker());
+    let cleanup_called = Cell::new(false);
+
+    {
+        let mut fut = pin!(list.insert_and_wait_with_cleanup(
+            node.as_mut(),
+            || cleanup_called.set(true),
+        ));
+        assert!(!cleanup_called.get());
+        // Poll so that the node attaches itself.
+        let _ = futures::poll!(fut.as_mut());
+        // Cause the node to become detached...
+        assert!(list.wake_one());
+        // ...and make it aware of this.
+        let _ = futures::poll!(fut);
+        // dropped
+    }
+    // Should not be called if the node was polled after detach.
+    assert!(!cleanup_called.get());
+}
+
+pub async fn test_iawwc_fire() {
+    create_list!(list);
+    // Drop list mutability. TODO: should create_list even return a Pin<&mut>?
+    let list = list.into_ref();
+
+    create_node!(node, (), lilos::exec::noop_waker());
+    let cleanup_called = Cell::new(false);
 
     // Testing the cleanup behavior is slightly subtle: we need to activate the
     // logic for the case where...
