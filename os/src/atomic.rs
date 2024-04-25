435578045bbf6e01b7a46fb1df5fe52dacba181f
Cliff L. Biffle <code@cliffle.com>
2024-04-25 16:48:31 -0700

    atomic: add fetch_update, use macros to reduce repetition

--- os/src/atomic.rs
+++ os/src/atomic.rs
@@ -26,13 +26,32 @@ pub trait AtomicExt {
 
     /// Atomically exchange our current contents for `val`, returning the
     /// original contents.
+    ///
+    /// Replacement for `swap`.
     fn swap_polyfill(&self, val: Self::Value, ordering: Ordering) -> Self::Value;
 
     /// If `self`'s value is equal to `current`, atomically replace it with
     /// `new`, otherwise leave it untouched.
     ///
     /// Returns `Ok(current)` on success, `Err(actual_value)` on failure.
+    ///
+    /// Replacement for `compare_exchange`.
     fn compare_exchange_polyfill(&self, current: Self::Value, new: Self::Value, success: Ordering, failure: Ordering) -> Result<Self::Value, Self::Value>;
+
+    /// Fetches the current value using `fetch_order`, applies `f` to it. If `f`
+    /// produces `Some(new_value)`, attempts to swap the value that was read for
+    /// `new_value`. If `f` produces `None`, stops.
+    ///
+    /// If the swap fails, the process repeats and `f` is called again with the
+    /// new observed value.
+    ///
+    /// Replacement for `fetch_update`.
+    fn fetch_update_polyfill(
+        &self,
+        set_order: Ordering,
+        fetch_order: Ordering,
+        f: impl FnMut(Self::Value) -> Option<Self::Value>
+    ) -> Result<Self::Value, Self::Value>;
 }
 
 /// Atomic operations that apply to arithmetic types.
@@ -43,61 +62,167 @@ pub trait AtomicArithExt: AtomicExt {
     fn fetch_or_polyfill(&self, val: Self::Value, ordering: Ordering) -> Self::Value;
 }
 
-#[cfg(feature = "has-native-rmw")]
-impl AtomicExt for AtomicU32 {
-    type Value = u32;
+macro_rules! impl_atomic_polyfills {
+    ($t:ty, $v:ty $(, $param:ident)?) => {
+        // Native version
+        #[cfg(feature = "has-native-rmw")]
+        impl<$($param)?> AtomicExt for $t {
+            type Value = $v;
 
-    fn swap_polyfill(&self, val: Self::Value, ordering: Ordering) -> Self::Value {
-        self.swap(val, ordering)
-    }
-    fn compare_exchange_polyfill(&self, current: Self::Value, new: Self::Value, success: Ordering, failure: Ordering) -> Result<Self::Value, Self::Value> {
-        self.compare_exchange(current, new, success, failure)
-    }
+            fn swap_polyfill(
+                &self,
+                val: Self::Value,
+                ordering: Ordering,
+            ) -> Self::Value {
+                self.swap(val, ordering)
+            }
+
+            fn compare_exchange_polyfill(
+                &self,
+                current: Self::Value,
+                new: Self::Value,
+                success: Ordering,
+                failure: Ordering,
+            ) -> Result<Self::Value, Self::Value> {
+                self.compare_exchange(current, new, success, failure)
+            }
+
+            fn fetch_update_polyfill(
+                &self,
+                set_order: Ordering,
+                fetch_order: Ordering,
+                f: impl FnMut(Self::Value) -> Option<Self::Value>,
+            ) -> Result<Self::Value, Self::Value> {
+                self.fetch_update(set_order, fetch_order, f)
+            }
+        }
+
+        // Non-native version
+        #[cfg(not(feature = "has-native-rmw"))]
+        impl<$($param)?> AtomicExt for $t {
+            type Value = $v;
+        
+            #[inline(always)]
+            fn swap_polyfill(
+                &self,
+                val: Self::Value,
+                ordering: Ordering,
+            ) -> Self::Value {
+                let (lo, so) = rmw_ordering(ordering);
+                cortex_m::interrupt::free(|_| {
+                    let x = self.load(lo);
+                    self.store(val, so);
+                    x
+                })
+            }
+
+            fn compare_exchange_polyfill(
+                &self,
+                current: Self::Value,
+                new: Self::Value,
+                success: Ordering,
+                failure: Ordering,
+            ) -> Result<Self::Value, Self::Value> {
+                let (lo, so) = rmw_ordering(success);
+                cortex_m::interrupt::free(|_| {
+                    let x = self.load(lo);
+                    if x == current {
+                        self.store(new, so);
+                        Ok(x)
+                    } else {
+                        self.store(x, failure);
+                        Err(x)
+                    }
+                })
+            }
+
+            fn fetch_update_polyfill(
+                &self,
+                set_order: Ordering,
+                fetch_order: Ordering,
+                mut f: impl FnMut(Self::Value) -> Option<Self::Value>
+            ) -> Result<Self::Value, Self::Value> {
+                let mut prev = self.load(fetch_order);
+                while let Some(next) = f(prev) {
+                    let cx_result = self.compare_exchange_polyfill(
+                        prev,
+                        next,
+                        set_order,
+                        fetch_order,
+                    );
+                    match cx_result {
+                        Ok(x) => return Ok(x),
+                        Err(change) => prev = change,
+                    }
+                }
+                Err(prev)
+            }
+        }
+    };
 }
 
-#[cfg(feature = "has-native-rmw")]
-impl AtomicArithExt for AtomicU32 {
-    fn fetch_add_polyfill(&self, val: Self::Value, ordering: Ordering) -> Self::Value {
-        self.fetch_add(val, ordering)
-    }
-    fn fetch_or_polyfill(&self, val: Self::Value, ordering: Ordering) -> Self::Value {
-        self.fetch_or(val, ordering)
-    }
+impl_atomic_polyfills!(AtomicU32, u32);
+impl_atomic_polyfills!(AtomicUsize, usize);
+impl_atomic_polyfills!(AtomicBool, bool);
+impl_atomic_polyfills!(AtomicPtr<T>, *mut T, T);
+
+
+macro_rules! impl_atomic_arith_polyfills {
+    ($t:ty $(, $param:ident)?) => {
+        // Native version
+        #[cfg(feature = "has-native-rmw")]
+        impl<$($param)?> AtomicArithExt for $t {
+            fn fetch_add_polyfill(
+                &self,
+                val: Self::Value,
+                ordering: Ordering,
+            ) -> Self::Value {
+                self.fetch_add(val, ordering)
+            }
+
+            fn fetch_or_polyfill(
+                &self,
+                val: Self::Value,
+                ordering: Ordering,
+            ) -> Self::Value {
+                self.fetch_or(val, ordering)
+            }
+        }
+
+        // Non-native version
+        #[cfg(not(feature = "has-native-rmw"))]
+        impl<$($param)?> AtomicArithExt for $t {
+            fn fetch_add_polyfill(
+                &self,
+                val: Self::Value,
+                ordering: Ordering,
+            ) -> Self::Value {
+                let (lo, so) = rmw_ordering(ordering);
+                cortex_m::interrupt::free(|_| {
+                    let x = self.load(lo);
+                    self.store(x.wrapping_add(val), so);
+                    x
+                })
+            }
+
+            fn fetch_or_polyfill(
+                &self,
+                val: Self::Value,
+                ordering: Ordering,
+            ) -> Self::Value {
+                let (lo, so) = rmw_ordering(ordering);
+                cortex_m::interrupt::free(|_| {
+                    let x = self.load(lo);
+                    self.store(x | val, so);
+                    x
+                })
+            }
+        }
+    };
 }
 
-#[cfg(feature = "has-native-rmw")]
-impl AtomicExt for AtomicUsize {
-    type Value = usize;
-
-    fn swap_polyfill(&self, val: Self::Value, ordering: Ordering) -> Self::Value {
-        self.swap(val, ordering)
-    }
-    fn compare_exchange_polyfill(&self, current: Self::Value, new: Self::Value, success: Ordering, failure: Ordering) -> Result<Self::Value, Self::Value> {
-        self.compare_exchange(current, new, success, failure)
-    }
-}
-
-#[cfg(feature = "has-native-rmw")]
-impl AtomicArithExt for AtomicUsize {
-    fn fetch_add_polyfill(&self, val: Self::Value, ordering: Ordering) -> Self::Value {
-        self.fetch_add(val, ordering)
-    }
-    fn fetch_or_polyfill(&self, val: Self::Value, ordering: Ordering) -> Self::Value {
-        self.fetch_or(val, ordering)
-    }
-}
-
-#[cfg(feature = "has-native-rmw")]
-impl<T> AtomicExt for AtomicPtr<T> {
-    type Value = *mut T;
-
-    fn swap_polyfill(&self, val: Self::Value, ordering: Ordering) -> Self::Value {
-        self.swap(val, ordering)
-    }
-    fn compare_exchange_polyfill(&self, current: Self::Value, new: Self::Value, success: Ordering, failure: Ordering) -> Result<Self::Value, Self::Value> {
-        self.compare_exchange(current, new, success, failure)
-    }
-}
+impl_atomic_arith_polyfills!(AtomicUsize);
+impl_atomic_arith_polyfills!(AtomicU32);
 
 #[cfg(not(feature = "has-native-rmw"))]
 #[inline(always)]
@@ -109,170 +234,5 @@ fn rmw_ordering(o: Ordering) -> (Ordering, Ordering) {
         Ordering::Acquire => (Ordering::Acquire, Ordering::Relaxed),
         Ordering::Release => (Ordering::Relaxed, Ordering::Release),
         _ => panic!(),
-    }
-}
-
-#[cfg(not(feature = "has-native-rmw"))]
-impl<T> AtomicExt for AtomicPtr<T> {
-    type Value = *mut T;
-
-    fn swap_polyfill(&self, val: Self::Value, ordering: Ordering) -> Self::Value {
-        let (lo, so) = rmw_ordering(ordering);
-        cortex_m::interrupt::free(|_| {
-            let x = self.load(lo);
-            self.store(val, so);
-            x
-        })
-    }
-    fn compare_exchange_polyfill(&self, current: Self::Value, new: Self::Value, success: Ordering, failure: Ordering) -> Result<Self::Value, Self::Value> {
-        let (lo, so) = rmw_ordering(success);
-        cortex_m::interrupt::free(|_| {
-            let x = self.load(lo);
-            if x == current {
-                self.store(new, so);
-                break Ok(x);
-            } else {
-                break Err(x);
-            }
-        });
-    }
-}
-
-#[cfg(not(feature = "has-native-rmw"))]
-impl AtomicExt for AtomicU32 {
-    type Value = u32;
-
-    #[inline(always)]
-    fn swap_polyfill(&self, val: Self::Value, ordering: Ordering) -> Self::Value {
-        let (lo, so) = rmw_ordering(ordering);
-        cortex_m::interrupt::free(|_| {
-            let x = self.load(lo);
-            self.store(val, so);
-            x
-        })
-    }
-    fn compare_exchange_polyfill(&self, current: Self::Value, new: Self::Value, success: Ordering, failure: Ordering) -> Result<Self::Value, Self::Value> {
-        let (lo, so) = rmw_ordering(success);
-        cortex_m::interrupt::free(|_| {
-            let x = self.load(lo);
-            if x == current {
-                self.store(new, so);
-                break Ok(x);
-            } else {
-                break Err(x);
-            }
-        });
-    }
-}
-
-#[cfg(not(feature = "has-native-rmw"))]
-impl AtomicArithExt for AtomicU32 {
-    #[inline(always)]
-    fn fetch_add_polyfill(&self, val: Self::Value, ordering: Ordering) -> Self::Value {
-        let (lo, so) = rmw_ordering(ordering);
-        cortex_m::interrupt::free(|_| {
-            let x = self.load(lo);
-            self.store(x.wrapping_add(val), so);
-            x
-        })
-    }
-
-    #[inline(always)]
-    fn fetch_or_polyfill(&self, val: Self::Value, ordering: Ordering) -> Self::Value {
-        let (lo, so) = rmw_ordering(ordering);
-        cortex_m::interrupt::free(|_| {
-            let x = self.load(lo);
-            self.store(x | val, so);
-            x
-        })
-    }
-}
-
-#[cfg(not(feature = "has-native-rmw"))]
-impl AtomicExt for AtomicUsize {
-    type Value = usize;
-
-    #[inline(always)]
-    fn swap_polyfill(&self, val: Self::Value, ordering: Ordering) -> Self::Value {
-        let (lo, so) = rmw_ordering(ordering);
-        cortex_m::interrupt::free(|_| {
-            let x = self.load(lo);
-            self.store(val, so);
-            x
-        })
-    }
-    fn compare_exchange_polyfill(&self, current: Self::Value, new: Self::Value, success: Ordering, failure: Ordering) -> Result<Self::Value, Self::Value> {
-        let (lo, so) = rmw_ordering(success);
-        cortex_m::interrupt::free(|_| {
-            let x = self.load(lo);
-            if x == current {
-                self.store(new, so);
-                break Ok(x);
-            } else {
-                break Err(x);
-            }
-        });
-    }
-}
-
-#[cfg(not(feature = "has-native-rmw"))]
-impl AtomicArithExt for AtomicUsize {
-    #[inline(always)]
-    fn fetch_add_polyfill(&self, val: Self::Value, ordering: Ordering) -> Self::Value {
-        let (lo, so) = rmw_ordering(ordering);
-        cortex_m::interrupt::free(|_| {
-            let x = self.load(lo);
-            self.store(x.wrapping_add(val), so);
-            x
-        })
-    }
-
-    #[inline(always)]
-    fn fetch_or_polyfill(&self, val: Self::Value, ordering: Ordering) -> Self::Value {
-        let (lo, so) = rmw_ordering(ordering);
-        cortex_m::interrupt::free(|_| {
-            let x = self.load(lo);
-            self.store(x | val, so);
-            x
-        })
-    }
-}
-
-#[cfg(feature = "has-native-rmw")]
-impl AtomicExt for AtomicBool {
-    type Value = bool;
-
-    fn swap_polyfill(&self, val: Self::Value, ordering: Ordering) -> Self::Value {
-        self.swap(val, ordering)
-    }
-    fn compare_exchange_polyfill(&self, current: Self::Value, new: Self::Value, success: Ordering, failure: Ordering) -> Result<Self::Value, Self::Value> {
-        self.compare_exchange(current, new, success, failure)
-    }
-}
-
-#[cfg(not(feature = "has-native-rmw"))]
-impl AtomicExt for AtomicBool {
-    type Value = bool;
-
-    #[inline(always)]
-    fn swap_polyfill(&self, val: Self::Value, ordering: Ordering) -> Self::Value {
-        let (lo, so) = rmw_ordering(ordering);
-        cortex_m::interrupt::free(|_| {
-            let x = self.load(lo);
-            self.store(val, so);
-            x
-        })
-    }
-    fn compare_exchange_polyfill(&self, current: Self::Value, new: Self::Value, success: Ordering, failure: Ordering) -> Result<Self::Value, Self::Value> {
-        let (lo, so) = rmw_ordering(success);
-        cortex_m::interrupt::free(|_| {
-            let x = self.load(lo);
-            if x == current {
-                self.store(new, so);
-                break Ok(x);
-            } else {
-                break Err(x);
-            }
-        });
     }
 }
