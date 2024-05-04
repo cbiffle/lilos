e130f5adaae1cac31ed9720d92dba840b408cf94
Cliff L. Biffle <code@cliffle.com>
2024-05-04 08:50:05 -0700

    msrv-all: tolerate packages without targets

--- msrv-all.sh
+++ msrv-all.sh
@@ -12,9 +12,15 @@ for d in $DIRS; do
         | jq -r '.resolve.root as $root | .packages[] | select(.id == $root) | .rust_version')
     TARGET=$(cargo metadata --format-version 1 \
         | jq -r '.resolve.root as $root | .packages[] | select(.id == $root) | .metadata.docs.rs["default-target"]')
-    echo "---- building in $d using ${VERSION} for ${TARGET}"
-    rustup update ${VERSION}
-    rustup target add --toolchain ${VERSION} ${TARGET}
-    rustup run ${VERSION} cargo check
+    if [[ "$TARGET" == "null" ]]; then
+        echo "---- building in $d using ${VERSION} (native)"
+        rustup update ${VERSION}
+        rustup run ${VERSION} cargo check
+    else
+        echo "---- building in $d using ${VERSION} for ${TARGET}"
+        rustup update ${VERSION}
+        rustup target add --toolchain ${VERSION} ${TARGET}
+        rustup run ${VERSION} cargo check
+    fi
     popd > /dev/null
 done
